9d23de724658df0bb4cab1ab034611afea8da710
Ramjoh <romjah@orange.fr>
2023-01-12 14:43:46 +0100

    Finish

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -11,7 +11,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
